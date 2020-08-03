ad76cb9c226d09c897ef1b737241eee7490d2c4b
VitaliiHamash <vitalii.m.hamash@gmail.com>
2020-08-03 10:10:38 -0700

    add navigation

---
+++ App.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React, { Component } from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    )
+  }
+}
+
+
+
+
+
+
